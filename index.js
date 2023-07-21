b5f06f2e1706ba3d25b6c01db1871696968cbf9d
brianbryank <brianbryan0125@gmail.com>
2023-07-21 15:01:08 +0300

    copleted

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+//let input
+let input;
+
 function addingEventListener() {
+  input = document.getElementById('input');
+  
+  input.addEventListener('change', inputChange);
+}
+
+function inputChange(event) {
+  console.log('Input value changed:', event.target.value);
 }
